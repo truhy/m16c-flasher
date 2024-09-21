8a6171db30500c68cb9aeb1c30f4efe1f62aec8b
Truong Hy <truhy7@gmail.com>
2024-09-21 14:00:55 +0100

    added write flash script examples for windows

---
+++ sourcecode/scripts_win/write flash example batch script/erase_write_flash_block_0_to_c.bat
@@ -0,0 +1,18 @@
+@ECHO OFF
+CALL env_win.bat
+
+:: Run
+SET runcmd=%APP% program path=%SERIALPATH% id_addr=0x0FFFDF id=00000000000000 erase=y from_addr=0x0C0000 to_addr=0x0FFFFF file="dump-block-0-to-c.bin"
+ECHO %runcmd%
+%runcmd% & IF %errorlevel% NEQ 0 GOTO :err_handler
+
+:: Pause if run from double-click
+IF /I %0 EQU "%~dpnx0" PAUSE
+
+GOTO :end_of_script
+
+:err_handler
+:: Pause if run from double-click
+IF /I %0 EQU "%~dpnx0" PAUSE
+
+:end_of_script
